07ad456195580334314f9f8f7b46b2077d38b3c7
sunnivalien <89248508+sunnivalien@users.noreply.github.com>
2022-01-27 17:52:21 +0100

    Update state_test.go

--- state/state_test.go
+++ state/state_test.go
@@ -10,3 +10,14 @@ func TestViewState(t *testing.T) {
          t.Errorf("Feil, fikk %q, ønsket %q.", state, wanted)
     }
 }
+
+func PutInBoat(item String)(
+  return "rev"
+}
+  func TestPutInBoat(t *testing.T) { 
+  wanted := "rev"
+  var state = PutInBoat()
+  if state != wanted {
+    t.Errorf( "Feil, fikk #{state}, ønsket #{wanted")
+  }
+}
