9b437b8f843ab688981f584544465523e0cfbbab
sunnivalien <89248508+sunnivalien@users.noreply.github.com>
2022-01-28 11:04:10 +0100

    commit on main

--- state/state_test.go
+++ state/state_test.go
@@ -1,23 +1,22 @@
- package state
+package state
 
 import "testing"
 
-
 func TestViewState(t *testing.T) {
-    wanted := "[kylling rev korn hs ---\\ \\__/ _________________/---]"
-    state := ViewState();
-    if state != wanted {
-         t.Errorf("Feil, fikk %q, ønsket %q.", state, wanted)
-    }
+	wanted := "[kylling rev korn hs ---\\ \\__/ _________________/---]"
+	state := ViewState()
+	if state != wanted {
+		t.Errorf("Feil, fikk %q, ønsket %q.", state, wanted)
+	}
 }
 
-func PutInBoat(item String)(
-  return "rev"
+func PutInBoat() string {
+	return "rev"
 }
-  func TestPutInBoat(t *testing.T) { 
-  wanted := "rev"
-  var state = PutInBoat()
-  if state != wanted {
-    t.Errorf( "Feil, fikk #{state}, ønsket #{wanted")
-  }
+func TestPutInBoat(t *testing.T) {
+	wanted := "rev"
+	var state = PutInBoat()
+	if state != wanted {
+		t.Errorf("Feil, fikk #{state}, ønsket #{wanted")
+	}
 }
