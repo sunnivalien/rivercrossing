811c4c2cf0f9e357f98803f005aaa92557117c85
sunnivalien <89248508+sunnivalien@users.noreply.github.com>
2022-01-28 11:39:35 +0100

    commit on main

--- state/state.go
+++ state/state.go
@@ -1,8 +1,14 @@
 package state
 
 func ViewState() string {
-  
-    return "[kylling rev korn hs ---\\ \\__/ _________________/---]"
+
+	return "[kylling rev korn hs ---\\ \\__/ _________________/---]"
 }
 
+func PutInBoat(dyr string) string {
+	return "[]" + dyr + "/---]"
 
+}
+func CrossRiver() string {
+	return "test"
+}
