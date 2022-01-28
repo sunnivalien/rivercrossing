132c8e4ebeaf51bfd73a564bb43e7097904355b0
sunnivalien <89248508+sunnivalien@users.noreply.github.com>
2022-01-28 11:53:02 +0100

    commit main

--- state/state.go
+++ state/state.go
@@ -1,14 +1,14 @@
 package state
 
 func ViewState() string {
-
+	// Sjekke data som er lagret ("kylling til venstre", "rev til venstre")
 	return "[kylling rev korn hs ---\\ \\__/ _________________/---]"
 }
 
-func PutInBoat(dyr string) string {
-	return "[]" + dyr + "/---]"
-
+func PutInBoat() string {
+	return "sau"
 }
+
 func CrossRiver() string {
 	return "test"
 }
--- state/state_test.go
+++ state/state_test.go
@@ -1,7 +1,9 @@
+// Package for testing the represention of the state of the riverworld
 package state
 
 import "testing"
 
+// Test function implemented in line with the Golang's testing tool
 func TestViewState(t *testing.T) {
 	wanted := "[kylling rev korn hs ---\\ \\__/ _________________/---]"
 	state := ViewState()
@@ -10,13 +12,10 @@ func TestViewState(t *testing.T) {
 	}
 }
 
-func PutInBoat() string {
-	return "rev"
-}
 func TestPutInBoat(t *testing.T) {
 	wanted := "rev"
-	var state = PutInBoat()
+	state := PutInBoat()
 	if state != wanted {
-		t.Errorf("Feil, fikk #{state}, ønsket #{wanted")
+		t.Errorf("Feil, fikk %q, ønsket %q.", state, wanted)
 	}
 }
